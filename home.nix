b43540e54b34bf4c0b84b594d7532decedfbff7e
Sebastian Wålinder <s.walinder@gmail.com>
2020-04-25 02:55:13 +0200

    Initial commit

---
+++ home.nix
@@ -0,0 +1,106 @@
+{ config, pkgs, ... }:
+
+{
+  programs.home-manager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Sebastian Wålinder";
+    userEmail = "s.walinder@gmail.com";
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.firefox = {
+    profiles.my.settings = {"extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";};
+
+    extensions = with pkgs.nur.repos.rycee.firefox-addons;
+      [
+        https-everywhere
+      ];
+  };
+
+  # nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
+
+  services.lorri.enable = true;
+
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "emacs";
+
+    # pinentry-program /run/current-system/sw/bin/pinentry-emacs
+    extraConfig = "allow-emacs-pinentry";
+    # grabKeyboardAndMouse = false;
+  };
+
+
+  home.packages = with pkgs; [
+    lm_sensors
+
+    ripgrep
+
+    atool unzip p7zip
+
+    direnv lorri
+
+    # mail
+    isync msmtp mu
+
+    gimp
+    redshift
+
+    firefox plasma-browser-integration w3m
+
+    shellcheck
+
+    cmake libtool gnumake
+
+    # pdftools
+    poppler
+
+    haskellPackages.structured-haskell-mode harfbuzz pinentry_emacs libvterm
+
+    # haskellPackages.glance - not on hackage
+    # haskellPackages.visualize-cbn - marked as broken
+
+    cloc
+
+    imagemagick
+
+    aspell aspellDicts.en aspellDicts.sv # languagetool # jre
+
+    mpv
+
+    # Needed by cabal?
+    binutils
+
+    # Haskell
+    ghc cabal-install hlint haskellPackages.hoogle
+
+    haskellPackages.hp2pretty # haskellPackages.threadscope # haskellPackages.eventlog2html
+
+    ormolu
+    nixfmt
+
+    nix-prefetch nix-prefetch-git nix-prefetch-github
+
+    # Used by hlint-refactor-mode
+    # haskellPackages.apply-refact
+
+    # Broken
+    # haskellPackages.halive
+
+    # haskellPackages.ghc-imported-from
+    # Applications
+
+    # Closed source
+    spotify
+  ];
+
+  home.stateVersion = "20.03";
+}
