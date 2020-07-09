021d20b54febdeb156090e7df3f40a4a9d027d17
Sebastian Wålinder <s.walinder@gmail.com>
2020-07-09 12:43:34 +0200

    Removed color

--- modules/visual.nix
+++ modules/visual.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 {
   # xsession.pointerCursor.size = 32;
-  xsession.pointerCursor.package = pkgs.capitaine-cursors;
+  # xsession.pointerCursor.package = pkgs.capitaine-cursors;
   # redglass or whiteglass
-  xsession.pointerCursor.name = "capitaine-cursors";
+  # xsession.pointerCursor.name = "capitaine-cursors";
 
-  xsession.pointerCursor.size = 32;
+  # xsession.pointerCursor.size = 32;
 
   fonts.fontconfig.enable = true;
 }
